c41a80c73b9669504c68f969592879e3645f408f
solitdio079 <solitdio079@gmail.com>
2024-11-29 10:45:14 +0300

    home is admin

--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -25,7 +25,7 @@ router.post(
 router.get(
   '/login/email/verify',
   passport.authenticate('magiclink', {
-    successReturnToOrRedirect: 'https://raltb.vercel.app/admin',
+    successReturnToOrRedirect: 'https://raltb.vercel.app/home',
     failureMessage: 'Token Invalide!',
   })
 )
