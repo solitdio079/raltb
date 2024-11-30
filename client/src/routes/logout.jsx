f4be5e80801388854e686c89aeee501623de247f
solitdio079 <solitdio079@gmail.com>
2024-11-30 08:40:31 +0300

    logout linked

--- client/src/routes/logout.jsx
+++ client/src/routes/logout.jsx
@@ -2,7 +2,7 @@ import { redirect } from 'react-router-dom'
 import { url } from '../utils/serverUrl'
 export async function action() {
   try {
-    const response = await fetch( url+ '/auth/logout', {
+    const response = await fetch( url + '/auth/logout', {
       method: 'POST',
       credentials: 'include',
       body: {},
